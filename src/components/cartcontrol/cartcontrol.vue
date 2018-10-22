f5310f889f39c512e6a017757af463d3ed4a682d
Ryan Lin <servant1001@gmail.com>
2018-10-23 00:59:08 +0800

    新增購物車小球動畫
    購物車小球動畫

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,19 +1,86 @@
 <template>
     <div class="cartcontrol">
-        
+        <transition name="move">
+            <div class="cart-decrease" v-show="food.count>0" @click.stop.prevent="decreaseCart">
+                <span class="inner icon-remove_circle_outline"></span>
+            </div>
+        </transition>
+        <div class="cart-count" v-show="food.count">{{food.count}}</div>
+        <div class="cart-add icon-add_circle" @click.stop.prevent="addCart"></div>
     </div>
 </template>
 
 <script>
+    import Vue from 'vue';
     export default {
         props: {
             food: {
                 type: Object
+            }
+        },
+        created() {
+            // console.log(this.food); // 檢查是否成功載入
+        },
+        methods: {
+            addCart(event) {
+                if (!event._constructed) { // 若不是我們開發的click直接忽略掉點擊(防止多點一次的情況發生)
+                    return;
+                }
+                // console.log('click'); // 檢查點擊
+                if (!this.food.count) {
+                    Vue.set(this.food, 'count', 1); // 在vue中，要給一個新的對象設置屬性的時候，要引入Vue然後用它的set(k, v, default)方法來設置，這樣該屬性才會被動態的檢測
+                } else {
+                    this.food.count++;
+                }
+                this.$emit('add', event.target);
+            },
+            decreaseCart(event) {
+                if (!event._constructed) {
+                    return;
+                }
+                if (this.food.count) {
+                    this.food.count--;
+                }
             }
         }
     };
 </script>
 
 <style lang="sass">
-
+    .cartcontrol
+        font-size: 0
+        .cart-decrease
+            display: inline-block
+            padding: 6px
+            opacity: 1
+            transform: translate3D(0, 0, 0)
+            .inner
+                display: inline-block
+                line-height: 24px
+                font-size: 24px
+                color: rgb(0,160,220)
+                transition: all 0.4s linear
+                transform: rotate(0)
+            &.move-enter-active, &.move-leave-active
+                transition: all 0.4s linear
+            &.move-enter, &.move-leave-active
+                opacity: 0
+                transform: translate3D(24px, 0, 0)
+                .inner
+                    transform: rotate(180deg)
+        .cart-count
+            display: inline-block
+            vertical-align: top
+            width: 12px
+            padding-top: 6px
+            line-height: 24px
+            text-align: center
+            font-size: 10px
+            color: rgb(147,153,159)
+        .cart-add
+            display: inline-block
+            padding: 6px
+            line-height: 24px
+            font-size: 24px
+            color: rgb(0, 160, 220)
 </style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,7 @@
 <template>
+  <div>
     <div class="goods">
-        <div class="menu-wrapper" ref="menuWrapper"><!--Vue1.x中使用v-el標記DOM元素，v-ref標記组件元素，2.0统一使用ref-->
+      <div class="menu-wrapper" ref="menuWrapper"><!--Vue1.x中使用v-el標記DOM元素，v-ref標記组件元素，2.0统一使用ref-->
             <ul>
                 <li v-for="(item,index) in goods" class="menu-item" ref="menuList" :class="{'current':currentIndex===index}" @click="selectMenu(index,$event)" :key="item.id">
                     <span class="text border-1px">
@@ -9,139 +10,166 @@
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper" ref="foodsWrapper">
+      <div class="foods-wrapper" ref="foodsWrapper">
+        <ul>
+          <li v-for="item in goods" class="food-list" ref="foodList" :key="item.id">
+            <h1 class="title">{{item.name}}</h1>
             <ul>
-                <li v-for="item in goods" class="food-list" ref="foodList" :key="item.id ">
-                    <h1 class="title">{{item.name}}</h1>
-                    <ul>
-                        <li v-for="food in item.foods" class="food-item border-1px" :key="food.id">
-                            <div class="icon">
-                                <img width="57px" height="57px" :src="food.icon" alt="">
-                            </div>
-                            <div class="content">
-                                <h2 class="name">{{food.name}}</h2>
-                                <p class="description">{{food.description}}</p>
-                                <div class="extra">
-                                    <span class="count">月售{{food.sellCount}}份</span>
-                                    <span>好評論{{food.rating}}%</span>
-                                </div>
-                                <div class="price">
-                                    <span class="now"><span class="CNY">¥</span> {{food.price}}</span>
-                                    <span class="old" v-show="food.oldPrice">¥ {{food.oldPrice}}</span>
-                                    <span class="TWD"> TWD {{food.price * 4.5}}</span>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </li>
+              <li v-for="food in item.foods" class="food-item border-1px" :key="food.id">
+                <div class="icon">
+                  <img width="57" height="57" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="description">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span><span class="old"
+                                                                  v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                  </div>
+                </div>
+              </li>
             </ul>
-        </div>
-        <shopcart :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
+          </li>
+        </ul>
+      </div>
+      <shopcart ref="shopcart" :selectFoods="selectFoods" :deliveryPrice="seller.deliveryPrice"
+                :minPrice="seller.minPrice"></shopcart>
     </div>
+    <!-- <food @add="addFood" :food="selectedFood" ref="food"></food> -->
+  </div>
 </template>
 
-<script>
-    import BScroll from 'better-scroll'; // 頁面滾動插件
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'; // 頁面滾動插件
     import shopcart from '../../components/shopcart/shopcart'; // 購物車組件
+    import cartcontrol from '../../components/cartcontrol/cartcontrol'; // 商品數量控制(-+)組建
+//   import food from '../../components/food/food';
 
-    const ERR_OK = 0;
-    // let data = require('../../../data.json');
-    export default {
-        props: {
-            seller: {
-                type: Object
+  const ERR_OK = 0;
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: [],
+        listHeight: [],
+        scrollY: 0,
+        selectedFood: {}
+      };
+    },
+    computed: {
+        currentIndex() { // 判斷目前落在哪個區間，回傳index
+            for (let i = 0; i < this.listHeight.length; i++) {
+                let height1 = this.listHeight[i];
+                let height2 = this.listHeight[i + 1];
+                if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+                    // this._followScroll(i);
+                    return i;
+                }
             }
+            return 0;
         },
-        data() {
-            return {
-                goods: [],
-                listHeight: [],
-                scrollY: 0,
-                selectedFood: {}
-            };
-        },
-        computed: {
-            currentIndex() { // 判斷目前落在哪個區間，回傳index
-                for (let i = 0; i < this.listHeight.length; i++) {
-                    let height1 = this.listHeight[i];
-                    let height2 = this.listHeight[i + 1];
-                    if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
-                        // this._followScroll(i);
-                        return i;
+        selectFoods() {
+            let foods = [];
+            this.goods.forEach((good) => {
+                good.foods.forEach((food) => {
+                    if (food.count) {
+                        foods.push(food);
                     }
-                }
-                return 0;
-            }
-        },
-        created() {
-            this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
-            this.$http.get('api/goods').then((response) => {
-                response = response.body;
-                if (response.errno === ERR_OK) {
-                    this.goods = response.data;
-                    this.$nextTick(() => { // $nextTick 是在下次DOM更新循環结束後執行延遲回調，在修改數據後使用$nextTick，可以在回調中獲取更新後的DOM，*這樣才可以確保執行函數是使用更新過的DOM
-                        this._initScroll();
-                        this._calculateHeight();
-                    });
-                }
-                // console.log(this.goods); // 查看是否有接收到goods資料
+                });
             });
-            // this.goods = data.goods;
-        },
-        methods: {
-            selectMenu(index, event) { // 點擊左邊商品列表時，右側滾動到對應商品位置
-                if (!event._constructed) { // 如果不存在這個屬性，則判斷為原生js點擊事件，不執行下面函数
-                    return;
-                }
-                let foodList = this.$refs.foodList;
-                let el = foodList[index];
-                this.foodsScroll.scrollToElement(el, 300);
-            },
-            selectFood(food, event) {
-                if (!event._constructed) {
-                    return;
-                }
-                this.selectedFood = food;
-                this.$refs.food.show();
-            },
-            _initScroll() {
-                this.meunScroll = new BScroll(this.$refs.menuWrapper, {
-                    click: true
-                });
-
-                this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
-                    click: true,
-                    probeType: 3
-                });
-
-                this.foodsScroll.on('scroll', (pos) => {
-                    // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
-                    if (pos.y <= 0) {
-                        this.scrollY = Math.abs(Math.round(pos.y));
-                    }
-                });
-            },
-            _calculateHeight() {
-                let foodList = this.$refs.foodList;
-                let height = 0;
-                this.listHeight.push(height);
-                for (let i = 0; i < foodList.length; i++) {
-                let item = foodList[i];
-                height += item.clientHeight;
-                this.listHeight.push(height);
-                }
-            }
-            // ,
-            // _followScroll(index) { // *左邊商品分類高量
-            //     let menuList = this.$refs.menuList;
-            //     let el = menuList[index];
-            //     this.meunScroll.scrollToElement(el, 300, 0, -100);
-            // }
-        },
-        components: { // 註冊組件
-            shopcart
+            return foods;
         }
-    };
+    },
+    created() {
+        this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+        this.$http.get('api/goods').then((response) => {
+            response = response.body;
+            if (response.errno === ERR_OK) {
+                this.goods = response.data;
+                this.$nextTick(() => { // $nextTick 是在下次DOM更新循環结束後執行延遲回調，在修改數據後使用$nextTick，可以在回調中獲取更新後的DOM，*這樣才可以確保執行函數是使用更新過的DOM
+                    this._initScroll();
+                    this._calculateHeight();
+                });
+            }
+            // console.log(this.goods); // 查看是否有接收到goods資料
+        });
+        // this.goods = data.goods;
+    },
+    methods: {
+        selectMenu(index, event) { // 點擊左邊商品列表時，右側滾動到對應商品位置
+            if (!event._constructed) { // 如果不存在這個屬性，則判斷為原生js點擊事件，不執行下面函数
+                return;
+            }
+            let foodList = this.$refs.foodList;
+            let el = foodList[index];
+            this.foodsScroll.scrollToElement(el, 300);
+        },
+        selectFood(food, event) {
+            if (!event._constructed) {
+                return;
+            }
+            this.selectedFood = food;
+            this.$refs.food.show();
+        },
+        addFood(target) {
+            this._drop(target);
+        },
+        _drop(target) {
+            // 購物車小球掉落動畫，異步執行動畫
+            this.$nextTick(() => {
+                this.$refs.shopcart.drop(target);
+            });
+        },
+        _initScroll() {
+            this.meunScroll = new BScroll(this.$refs.menuWrapper, {
+                click: true
+            });
+
+            this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+                click: true,
+                probeType: 3
+            });
+
+            this.foodsScroll.on('scroll', (pos) => {
+                // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+                if (pos.y <= 0) {
+                    this.scrollY = Math.abs(Math.round(pos.y));
+                }
+            });
+        },
+        _calculateHeight() {
+            let foodList = this.$refs.foodList;
+            let height = 0;
+            this.listHeight.push(height);
+            for (let i = 0; i < foodList.length; i++) {
+            let item = foodList[i];
+            height += item.clientHeight;
+            this.listHeight.push(height);
+            }
+        }
+        // ,
+        // _followScroll(index) { // *左邊商品分類高量
+        //     let menuList = this.$refs.menuList;
+        //     let el = menuList[index];
+        //     this.meunScroll.scrollToElement(el, 300, 0, -100);
+        // }
+    },
+    components: { // 註冊組件
+        shopcart,
+        cartcontrol
+    }
+  };
 </script>
 
 <style lang="sass">
@@ -252,4 +280,8 @@
                             color: rgb(147,153,159)
                         .CNY
                             font-size: 10px
+                    .cartcontrol-wrapper
+                        position: absolute
+                        right: 0
+                        bottom: 12px
 </style>
